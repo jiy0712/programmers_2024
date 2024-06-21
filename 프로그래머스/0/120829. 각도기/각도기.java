f376d6b5051b41d7a800ad11c48a5f7d2a183872
박지영 <s2325@e-mirim.hs.kr>
2024-06-21 13:45:43 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int angle) {
+        if (angle > 0 && angle < 90) {
+            return 1; // 예각
+        } else if (angle == 90) {
+            return 2; // 직각
+        } else if (angle > 90 && angle < 180) {
+            return 3; // 둔각
+        } else if (angle == 180) {
+            return 4; // 평각
+        } else {
+            throw new IllegalArgumentException("유효하지 않은 각도입니다.");
+        }
+    }
+}
