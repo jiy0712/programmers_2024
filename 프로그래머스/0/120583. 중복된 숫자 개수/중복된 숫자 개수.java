e11328c4c98bf3a9adf4bd22e0a9b6c5e6dcc8a6
박지영 <s2325@e-mirim.hs.kr>
2024-06-24 12:55:29 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int count = 0;
+                for (int num : array) {
+            if (num == n) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+    }
+}
