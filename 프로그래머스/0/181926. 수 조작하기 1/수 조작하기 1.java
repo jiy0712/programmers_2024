61beb8a2c1d269661d6fcb8174b5e3f96c82dcfd
박지영 <s2325@e-mirim.hs.kr>
2024-06-25 13:01:18 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.07 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n, String control) {
+        for (char c : control.toCharArray()) {
+            if (c == 'w') {
+                n += 1;
+            } else if (c == 's') {
+                n -= 1;
+            } else if (c == 'd') {
+                n += 10;
+            } else if (c == 'a') {
+                n -= 10;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+     
+    }
+}
