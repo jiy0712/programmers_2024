9bc1f9d3a45c3f35686aa3251daf1382c359af42
박지영 <s2325@e-mirim.hs.kr>
2024-06-25 13:06:25 +0900

    [level 0] Title: n의 배수, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int num, int n) {
+        if (num % n == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        
+    }
+}
