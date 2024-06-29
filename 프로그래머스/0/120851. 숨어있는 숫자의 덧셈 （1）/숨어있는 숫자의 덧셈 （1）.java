4d8a787bfc94332d8667bce5f46553b9a15c6860
박지영 <s2325@e-mirim.hs.kr>
2024-06-29 20:23:57 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.04 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solution(String my_string) {
+        int sum = 0;
+        for (char ch : my_string.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                sum += Character.getNumericValue(ch);
+            }
+        }
+        return sum;
+    }
+}
