90db4d400cb7727b859551315fa1443f9d91450c
박지영 <s2325@e-mirim.hs.kr>
2024-07-09 19:30:05 +0900

    [level 0] Title: 수 조작하기 2, Time: 8.61 ms, Memory: 94.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i < numLog.length; i++) {
+            int diff = numLog[i] - numLog[i - 1];
+            switch (diff) {
+                case 1:
+                    result.append("w");
+                    break;
+                case -1:
+                    result.append("s");
+                    break;
+                case 10:
+                    result.append("d");
+                    break;
+                case -10:
+                    result.append("a");
+                    break;
+            }
+        }
+        return result.toString();
+    }
+}
