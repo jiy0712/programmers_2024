9dd7a669cd2be0d1599b4ba7914a20e6e58098f6
박지영 <s2325@e-mirim.hs.kr>
2024-07-13 10:20:42 +0900

    [level 0] Title: 2의 영역, Time: 2.32 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                start = i;
+                break;
+            }
+        }
+        
+        if (start == -1) {
+            return new int[]{-1};
+        }
+        
+        for (int i = arr.length - 1; i >= start; i--) {
+            if (arr[i] == 2) {
+                end = i;
+                break;
+            }
+        }
+        
+        int[] result = new int[end - start + 1];
+        for (int i = start; i <= end; i++) {
+            result[i - start] = arr[i];
+        }
+        
+        return result;
+    }
+}
