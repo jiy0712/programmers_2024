4a52e9186747b148f2718fe1a89d449dea18a957
박지영 <s2325@e-mirim.hs.kr>
2024-07-14 22:34:39 +0900

    [level 0] Title: 배열 만들기 4, Time: 78.32 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        
+        int i = 0;
+        while (i < arr.length) {
+            if (stk.isEmpty() || stk.peek() < arr[i]) {
+                stk.push(arr[i]);
+                i++;
+            } else {
+                stk.pop();
+            }
+        }
+        
+        int[] result = new int[stk.size()];
+        for (int j = 0; j < result.length; j++) {
+            result[j] = stk.get(j);
+        }
+        
+        return result;
+    }
+}
