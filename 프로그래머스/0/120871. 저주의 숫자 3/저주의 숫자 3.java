46fd45bf673ee46ba0de1f4bc21bca0ae7ff4474
박지영 <s2325@e-mirim.hs.kr>
2024-07-15 16:26:53 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.27 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int count = 0;
+        
+        for (int i = 1; count < n; i++) {
+            if (i % 3 != 0 && !String.valueOf(i).contains("3")) {
+                count++;
+                answer = i;
+            }
+        }
+        
+        return answer;
+    }
+}
