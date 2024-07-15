d90a02b0a1af404fbe339fa5cd510915778a01ff
박지영 <s2325@e-mirim.hs.kr>
2024-07-15 22:46:16 +0900

    [level 0] Title: 글자 지우기, Time: 0.63 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        Arrays.sort(indices);
+        
+        StringBuilder result = new StringBuilder(my_string);
+        
+        for (int i = indices.length - 1; i >= 0; i--) {
+            result.deleteCharAt(indices[i]);
+        }
+        
+        return result.toString();
+    }
+}
