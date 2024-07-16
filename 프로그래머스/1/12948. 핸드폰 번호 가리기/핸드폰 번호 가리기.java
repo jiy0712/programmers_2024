e32ecc37694f13e57ccf28ee5532a0a670b0a266
박지영 <s2325@e-mirim.hs.kr>
2024-07-16 14:58:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.48 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String phone_number) {
+        int length = phone_number.length();
+        String hiddenPart = "*".repeat(length - 4);
+        String visiblePart = phone_number.substring(length - 4);
+        return hiddenPart + visiblePart;
+    }
+}
