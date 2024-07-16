fdf24fac221bba49f90ee3953fe750a3529c2af1
박지영 <s2325@e-mirim.hs.kr>
2024-07-16 20:30:24 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 5.36 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        HashMap<Character, Integer> lastSeen = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (lastSeen.containsKey(currentChar)) {
+                answer[i] = i - lastSeen.get(currentChar);
+            } else {
+                answer[i] = -1;
+            }
+            lastSeen.put(currentChar, i);
+        }
+        
+        return answer;
+    }
+}
